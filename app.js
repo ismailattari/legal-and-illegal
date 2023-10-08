f4afe800a265e283b44c704794d7d8a00f9a6f16
ismailattari <136378019+ismailattari@users.noreply.github.com>
2023-10-08 10:51:52 +0500

    legal-and-illegal

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("my name is ismail");
+alert("my rool number is cco 117469");
+
+
+var $name="$name is legal "
+var _name=" _name is legal"
+var name1=" name is legal"
+alert($name+ _name+ name1);
+
+
+var legal = " this is legal\n letters,numbers,dollar signs,and underscores";
+alert(legal)
+
+var illegal = "this is illegal\n any spaces, js keywords,starting with numbers,";
+alert(illegal);
